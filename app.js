4112a2463c3bd09018aed1b10d93303b0dec608b
erinla <erin_lawler@yahoo.com>
2019-08-12 16:50:14 -0500

    finished

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
 function dropdown() {
     document.getElementById("inventory").classList.toggle("show");
 }
-
-window.onclick = function (e) {
-    if (!e.target.matches('.dropbtn')) {
-        var myDropdown = document.getElementById("myDropdown");
-        if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show');
-        }
-    }
-}
